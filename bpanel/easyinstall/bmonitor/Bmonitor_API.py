7b747ec49543f36f929308fb24f580f806f62045
root <root@django.domain.name>
2013-05-15 10:15:33 -0400

    dia 15/05

---
+++ bpanel/easyinstall/bmonitor/Bmonitor_API.py
@@ -0,0 +1,9 @@
+# Bmonitor APIy
+import json
+
+def requestBMONData( jsonreq ):
+	print  jsonreq 
+	return jsonreq
+	
+#requestZBXData( '{"jsonrpc":"2.0","method":"user.login","params":{"user":"teste","password":"teste"},"id":1}' )
+#http://172.16.1.114/easyinstall/zbxreq/?jsonreq={"jsonrpc":"2.0","method":"user.login","params":{"user":"dartanghan","password":"bouwen$001"},"id":1}
